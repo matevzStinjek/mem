a1f679ea75d06a4a3006abc47f2bdb29c43e76ec
Matevz <stinjek@gmail.com>
2019-08-15 20:13:42 +0200

    upgraded packages

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+const API_PREFIX = '/api';
+
 // =============================
 // format: $route => $controller
 // =============================
@@ -9,5 +11,8 @@ $routes = [
 ];
 
 foreach($routes as $route => $controller) {
-    $app->any("/api$route","App\\Controllers\\$controller:handleRequest");
+    $app->any(API_PREFIX . $route, "App\\Controllers\\$controller:handleRequest");
 }
+
+$app->any('/api/test', 'App\\Action\\PhotoAction:fetch');
+$app->any('/api/test/{slug}', 'App\\Action\\PhotoAction:fetchOne');
