0d2109564c2ab141436a76cc06a7c9660bb4bf87
Matevz <stinjek@gmail.com>
2019-08-18 23:16:53 +0200

    dotenv

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@ require __DIR__ . '/../vendor/autoload.php';
 
 session_start();
 
+$dotenv = Dotenv\Dotenv::create(__DIR__ . '/..');
+$dotenv->load();
+
 // Instantiate the app
 $settings = require __DIR__ . '/../app/settings.php';
 $app = new \Slim\App($settings);
