2abfb61191249f50401f5f243ad2ae2c50a67bc7
Matevz <stinjek@gmail.com>
2019-08-24 00:21:38 +0200

    vuex fix

--- client/src/store/modules/shared/mutations.js
+++ client/src/store/modules/shared/mutations.js
@@ -1,2 +1,14 @@
+import { getState } from './state'
+
+const getResetState = (currentState, excludedStateKeys) => {
+    const resetState = getState()
+
+    for (const key of excludedStateKeys) {
+        resetState[key] = currentState[key]
+    }
+
+    return resetState
+}
+
 export default {
 }
