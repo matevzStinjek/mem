e2ec9487ab3f49807cde26e62970ec14299df893
Matevz <stinjek@gmail.com>
2019-08-24 14:10:29 +0200

    wip request proxying - not working as is

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,9 +1,20 @@
 const path = require('path')
 
 module.exports = {
+    publicPath: '/',
+    outputDir: '../server/public/assets',
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8081',
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
     chainWebpack: config => {
         config.resolve.alias
             .set('design-system', path.resolve(__dirname, 'src/components/design_system'))
-            .set('api-client', path.resolve(__dirname, 'src/apiClient.js'))
+            .set('api-client',    path.resolve(__dirname, 'src/apiClient.js'))
     },
 }
