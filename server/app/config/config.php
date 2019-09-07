79782c9fc4346d3a862e0c7859610f35edf4eb68
Matevz <stinjek@gmail.com>
2019-09-07 16:41:35 +0200

    initial refactor

---
+++ server/app/config/config.php
@@ -0,0 +1,38 @@
+<?php
+return [
+    'settings' => [
+        'doctrine' => [
+            'meta' => [
+                'entity_path' => [
+                    'app/src/Entity'
+                ],
+                'auto_generate_proxies' => true,
+                'proxy_dir' =>  __DIR__.'/../cache/proxies',
+                'cache' => null,
+            ],
+            'connection' => [
+                'driver'   => 'pdo_mysql',
+                'host'     => '127.0.0.1',
+                'port'     => '3306',
+                'dbname'   => 'mem',
+                'user'     => getenv('DB_USER'),
+                'password' => getenv('DB_PASSWORD'),
+            ],
+        ],
+        'aws' => [
+            's3' => [
+                'meta' => [
+                    'bucket' => 'mem-buckoo',
+                ],
+                'connection' => [
+                    'region'      => 'us-east-1',
+                    'version'     => 'latest',
+                    'credentials' => [
+                        'key'    => getenv('AWS_KEY'),
+                        'secret' => getenv('AWS_SECRET'),
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
--- server/todo.md
+++ server/todo.md
@@ -1,9 +1,16 @@
-SUNDAY:
+NEXT PHASE:
+* upgrade to slim 4
+* refactor dependencies to $app (extend SlimApp to my App :) )s
+* filter chain type auth
+* custom Request wrapper ($request = new Request(SlimRequest $slimRequest);) that adds user(auth), params, em
+* filter in routes (middleware)
+
+PHASE + 1:
 * usersgroups <> foldersMembership <> folder
 * blob <> assetcontroller <> foldercontent relationship
 * UserGroupController.php && Resource
 
-SOMEDAY:
+PHASE + 2:
 * rename UserGroup to UsersGroup and UserGroupMemberships to UsersGroupsMemberships
 * advanced permissions
 * rest of the fucking api
