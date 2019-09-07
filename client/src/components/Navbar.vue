df806b4cdd140bb5f83cf19d4cb37d993685cd49
Matevz <stinjek@gmail.com>
2019-09-07 15:21:52 +0200

    some basic structure

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="navbar">
+        navbar
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="less" scoped>
+@import '~@/common/less/common';
+
+.navbar {
+    position: sticky;
+    top: 0;
+    height: 48px;
+    border-bottom: 1px solid @white;
+    background-color: @main;
+}
+</style>
--- client/src/views/error/404View.vue
+++ client/src/views/error/404View.vue
@@ -3,8 +3,3 @@
         404 Not found
     </div>
 </template>
-
-<script>
-export default {
-}
-</script>
