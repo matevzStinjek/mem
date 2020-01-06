5d0a059e089e5469fd2e00cb770e368e889f12ed
Matevz <stinjek@gmail.com>
2020-01-07 00:24:27 +0100

    sessions db

--- server/app/database/db.sql
+++ server/app/database/db.sql
@@ -4,10 +4,23 @@ CREATE TABLE users (
     email VARCHAR(255) COLLATE utf8_unicode_ci,
     passwordHash CHAR(64) COLLATE utf8_unicode_ci,
     salt CHAR(16) COLLATE utf8_unicode_ci,
-    roles longblob COLLATE utf8_unicode_ci,
+    roles LONGBLOB COLLATE utf8_unicode_ci,
     creationTimestamp DATETIME NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE sessions (
+    id varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+    userId INT UNSIGNED NOT NULL,
+    creationTimestamp datetime NOT NULL,
+    lastActivityTimestamp datetime NOT NULL,
+    httpUserAgent text COLLATE utf8_unicode_ci DEFAULT NULL,
+    persistConfirmed tinyint(1) NOT NULL DEFAULT '0',
+    PRIMARY KEY (id),
+    KEY sessions_userId_k (userId),
+    KEY sessions_persistConfirmed_lastActivityTimestamp_k (persistConfirmed, lastActivityTimestamp),
+    CONSTRAINT sessions_userId_fk FOREIGN KEY (userId) REFERENCES users (id)
+)
 
 INSERT INTO users VALUES(0, 'admin', 'admin@mem.com', '259926f400eb547cd6b5dea51e50291f2e1d8ed8df452b7c9646ef9a8862d874', '$salt', 'ADMIN', NOW());
 
@@ -65,38 +78,3 @@ CREATE TABLE folderContent (
 
 -- blob hash
 INSERT INTO folderContent VALUES('199f25fd69940560d438a6d7d3ace16e3cb4eab9cf2ad6ff069ccdc0585bb8b3', 1, 1, NOW());
-
--- TODO
-CREATE TABLE sessions (
-    id INT CHARACTER SET ascii NOT NULL,
-    userId INT CHARACTER SET ascii NOT NULL,
-    lastActivityTimestamp datetime DEFAULT NULL,
-    creationTimestamp datetime NOT NULL,
-    PRIMARY KEY (id),
-    KEY sessions_creationTimestamp_k (creationTimestamp),
-    KEY sessions_userId_k (userId),
-    CONSTRAINT sessions_userId_fk FOREIGN KEY (userId) REFERENCES users (id)
-)
-
-CREATE TABLE sessions (
-    id varchar(64) COLLATE utf8_unicode_ci NOT NULL,
-    userId char(8) CHARACTER SET ascii DEFAULT NULL,
-    creationTimestamp datetime NOT NULL,
-    lastActivityTimestamp datetime NOT NULL,
-    httpUserAgent text COLLATE utf8_unicode_ci,
-    persistConfirmed tinyint(1) NOT NULL DEFAULT '0',
-    lastSsoValidationTimestamp datetime DEFAULT NULL,
-    lastLoginValidationTimestamp datetime DEFAULT NULL,
-    facebookDistributionAccessToken text COLLATE utf8_unicode_ci,
-    facebookAuthorizationAccessToken text COLLATE utf8_unicode_ci,
-    twitterDistributionAccessToken text COLLATE utf8_unicode_ci,
-    twitterAuthorizationAccessToken text COLLATE utf8_unicode_ci,
-    snapchatDistributionAccessToken text COLLATE utf8_unicode_ci,
-    snapchatAuthorizationAccessToken text COLLATE utf8_unicode_ci,
-    sizmekDistributionAccessToken text COLLATE utf8_unicode_ci,
-    youtubeAuthorizationAccessToken text COLLATE utf8_unicode_ci,
-    PRIMARY KEY (id),
-    KEY sessions_userId_k (userId),
-    KEY sessions_persistConfirmed_lastActivityTimestamp_k (persistConfirmed,lastActivityTimestamp),
-    CONSTRAINT sessions_userId_fk FOREIGN KEY (userId) REFERENCES users (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
